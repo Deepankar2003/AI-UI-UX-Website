efa43a6684d93889c446f4294d87d480b280d804
Deepankar2003 <sdeepankar42@gmail.com>
2024-03-05 00:55:34 +0530

    2nd commit

--- src/components/Benefits.jsx
+++ src/components/Benefits.jsx
@@ -4,6 +4,7 @@ import Section from "./Section";
 import Arrow from "../assets/svg/Arrow";
 import { GradientLight } from "./design/Benefits";
 import ClipPath from "../assets/svg/ClipPath";
+{/* <h1>hello</h1> */}
 
 const Benefits = () => {
   return (
